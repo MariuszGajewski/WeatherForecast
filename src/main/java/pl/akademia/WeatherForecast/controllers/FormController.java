82755582f0318891916f2491de42643f41d481a2
Mariusz Gajewski <mariusz.gajewski89@gmail.com>
2018-03-08 15:03:35 +0100

    Initial commit

---
+++ src/main/java/pl/akademia/WeatherForecast/controllers/FormController.java
@@ -0,0 +1,29 @@
+package pl.akademia.WeatherForecast.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.akademia.WeatherForecast.models.WeatherModel;
+import pl.akademia.WeatherForecast.models.services.WeatherService;
+
+import java.util.List;
+
+@Controller
+public class FormController {
+    private WeatherService weatherService = WeatherService.getInstance();
+
+    @GetMapping("/weather")
+    public String weatherForm() {
+        return "weather";
+    }
+
+    @PostMapping("/getWeatherController")
+    public String weatherForm(@RequestParam("cityName") String cityName, @RequestParam("weatherType") String weatherType, Model model) {
+        List<WeatherModel> weatherModel=null;
+        weatherModel=weatherType.equals("actual") ? weatherService.getWeather(cityName) : weatherService.getWeatherForecast(cityName);
+        model.addAttribute("weathers",weatherModel);
+        return "weather";
+    }
+}
